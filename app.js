9a28bb2b66b1a54be5db9f4567a969fc87fd994a
raihan <raihanulmcse@mail.com>
2022-02-28 15:33:49 +0800

    modified

--- app.js
+++ app.js
@@ -1,8 +1,54 @@
 const allPlayers = () => {
+	document.getElementById;
 	const searchBox = document.getElementById("search-box").value;
 	const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchBox}`;
 	fetch(url)
 		.then((res) => res.json())
-		.then((data) => console.log(data));
-	console.log(searchBox);
+		.then((data) => showPleayerDetails(data.player));
+};
+
+const showPleayerDetails = (players) => {
+	for (const player of players) {
+		const parent = document.getElementById("player-container");
+
+		const div = document.createElement("div");
+		div.innerHTML = `	<div class="card border p-5">
+    <div class="pro-pic">
+        <img  class="w-25" src=" ${player.strThumb}" alt="">
+    </div>
+    <h2>Name: ${player.strPlayer}</h2>
+    <h4>Country: ${player.strNationality}</h4>
+    <div class="all-button">
+        <button class="btn btn-danger">Delete</button>
+        <button onclick="details('${player.idPlayer}')" class="btn btn-success">Details</button>
+    </div>
+
+</div>`;
+		parent.appendChild(div);
+	}
+};
+
+const details = (id) => {
+	const url = `https://www.thesportsdb.com/api/v1/json/2/lookupplayer.php?id=${id}`;
+	fetch(url)
+		.then((res) => res.json())
+		.then((data) => setDetails(data.players[0]));
+};
+
+const setDetails = (info) => {
+	if (info.strGender == "Male") {
+		document.getElementById("male").style.display = "block";
+		document.getElementById("female").style.display = "none";
+	} else {
+		document.getElementById("male").style.display = "none";
+		document.getElementById("female").style.display = "block";
+	}
+	document.getElementById("deatails-container").innerHTML = `
+                    <div>            
+                    <img  class="w-25" src=" ${info.strThumb}" alt="">
+					<h1>Name:${info.strPlayer}</h1>	
+					<h1>Country:${info.strNationality}</h1>	
+                    <h3>Id: ${info.idPlayer}</h3>
+					</div>
+ `;
 };
